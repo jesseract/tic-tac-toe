0f44b8717366f128a8edd9aecc417f66d25fe08e
Jesseract <jlpearce@gmail.com>
2015-07-23 15:55:23 -0400

    Unbeatable using rules

--- computer_player.rb
+++ computer_player.rb
@@ -17,11 +17,33 @@ class ComputerPlayer
       return square
     end
 
-    #if human_player can fork, stop them
-    square = square_that_lets_player_fork(board, game, game.human_player)
-    puts "can prevent fork? #{square}"
-    if square != nil
-      return square
+    #check for ways we can be forked
+    forking_squares = squares_that_let_player_fork(board, game, game.human_player)
+    puts "forking squares: #{forking_squares}"
+    if forking_squares.length == 1
+      #if human_player can fork, stop them
+      puts "can prevent fork: #{square}"
+      return forking_squares[0]
+    elsif forking_squares.length > 1
+      #if we detect two possible forks on edges, play a random open corner
+      if (forking_squares & edges).length != 0
+        #play corner if not occupied
+        square = open_corner_square(board)
+        puts "can prevent double fork at corner? #{square}"
+        if square != nil
+          return square
+        end
+      end
+
+      #if we detect two possible forks on corners, play a random open edge
+      if (forking_squares & corners).length != 0
+        #play edge if not occupied
+        square = open_edge_square(board)
+        puts "can prevent double fork at edge? #{square}"
+        if square != nil
+          return square
+        end
+      end
     end
 
     #play center if not occupied
@@ -57,7 +79,7 @@ class ComputerPlayer
     return nil
   end
 
-  def square_that_lets_player_fork(board, game, player)
+  def squares_that_let_player_fork(board, game, player)
     moves = game.moves_for_player(player)
     #prevent a fork, which is when a player has two win cases, each with one occupied square, that intersect at an unoccupied square
 
@@ -77,6 +99,7 @@ class ComputerPlayer
 
     #consider every combination of two win cases
     #outer loop looks at the entire contents of lines excluding the last one
+    forking_squares = []
     for first_index in 0...(lines.length - 1)
       first_line = lines[first_index]
       #inner loop takes every line after the first and considers it against each other line
@@ -85,17 +108,24 @@ class ComputerPlayer
         intersection = first_line & second_line
         #sees if they intersect, looks to see if the intersection is blank, if blank, returns that square
         if intersection.length == 1 && board.can_play_at?(intersection[0])
-          return intersection[0]
+          forking_squares << intersection[0]
         end
       end
     end
-    #if there is no fork, return nil
-    return nil
+
+    return forking_squares.uniq
   end
 
   def open_corner_square(board)
-    corners = [1, 3, 7, 9]
-    corners.each do |square|
+    open_square_among_squares(board, corners)
+  end
+
+  def open_edge_square(board)
+    open_square_among_squares(board, edges)
+  end
+
+  def open_square_among_squares(board, squares)
+    squares.each do |square|
       if board.can_play_at?(square)
         return square
       end
@@ -112,59 +142,12 @@ class ComputerPlayer
     return square
   end
 
-    # if board.total_moves == 0
-    #   return 5
-    # end
+  def corners
+    return [1, 3, 7, 9]
+  end
 
-    # #pick a good move if there are any good moves, otherwise just pick a move
-    # computer_is_x = (game.first_player == self)
-    #
-    # rated_choices = rate_choices(board, computer_is_x)
-    # rated_choices.each do |choice, is_good|
-    #   if is_good
-    #     return choice
-    #   end
-    # end
-    # return rated_choices.keys[0]
+  def edges
+    return [2, 4, 6, 8]
+  end
 
-
-  # def rate_choices(board, computer_is_x)
-  #   #returns a hash of moves and whether they're good or bad. Keys are fixnums,
-  #   # values are boolean
-  #   # look at the numbers 0-8 and ask the board 'can I play there' return a hash
-  #   rated_choices = {}
-  #   for choice in 1..9
-  #     if board.can_play_at?(choice)
-  #       rated_choices[choice] = good_board?(board.board_with_move(choice), computer_is_x)
-  #     end
-  #   end
-  #   return rated_choices
-  # end
-  #
-  # def good_board?(board, computer_is_x)
-  #   #decide whether a board is good or bad for the computer_player
-  #   if board.game_over?
-  #     #If we have won or tied, that is good. If we have lost, that's bad
-  #     result = board.result
-  #     if result == :draw
-  #       return true
-  #     elsif result == :X
-  #       return computer_is_x
-  #     else
-  #       return !computer_is_x
-  #     end
-  #   else
-  #     # if game is not over, look at all the moves we could make to see if we or
-  #     # the other player is forced
-  #     rated_choices = rate_choices(board, computer_is_x)
-  #     if board.is_x_turn? == computer_is_x
-  #       #if it's computer's turn, the board is good if I have any good choices, otherwise we are forced which is bad
-  #       return rated_choices.values.include?(true)
-  #     else
-  #       #opponent's turn, the board is bad if any choices are bad for computer_player, otherwise they are forced which is good
-  #       return !rated_choices.values.include?(false)
-  #     end
-  #   end
-  #
-  # end
 end
