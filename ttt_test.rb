16273212df065efba9e05892513349d427ab024f
Jesseract <jlpearce@gmail.com>
2015-07-23 22:19:06 -0400

    Complete test suite for computer_player class

--- ttt_test.rb
+++ ttt_test.rb
@@ -16,7 +16,7 @@ class TicTacToeTest < Minitest::Test
     assert HumanPlayer
   end
 
-  def test_03_empty_board_can_display_itself
+  def test_02_empty_board_can_display_itself
     board = Board.new
   end
 
@@ -31,12 +31,12 @@ class TicTacToeTest < Minitest::Test
 }
   end
 
-  def test_04_computer_plays_center_on_blank_board
+  def test_03_computer_plays_center_on_blank_board
     board = Board.new
     assert_equal ComputerPlayer.new.choose_square(board), 5
   end
 
-  def test_05_x_can_win
+  def test_04_x_can_win
     board = Board.new
     board = board.board_with_move(5)
     board = board.board_with_move(2)
@@ -47,7 +47,7 @@ class TicTacToeTest < Minitest::Test
     assert_equal :X, board.result
   end
 
-  def test_06_o_can_win
+  def test_05_o_can_win
     board = Board.new
     board = board.board_with_move(2)
     board = board.board_with_move(5)
@@ -58,7 +58,7 @@ class TicTacToeTest < Minitest::Test
     assert_equal :O, board.result
   end
 
-  def test_07_computer_will_prevent_fork
+  def test_06_computer_will_prevent_fork
     board = Board.new
     board = board.board_with_move(7)
     board = board.board_with_move(5)
@@ -66,7 +66,7 @@ class TicTacToeTest < Minitest::Test
     assert_equal 1, ComputerPlayer.new.choose_square(board)
   end
 
-  def test_08_computer_will_play_corner_if_forked_on_edge
+  def test_07_computer_will_play_corner_if_forked_on_edge
     board = Board.new
     board = board.board_with_move(5)
     board = board.board_with_move(1)
@@ -74,7 +74,7 @@ class TicTacToeTest < Minitest::Test
     assert_equal 3, ComputerPlayer.new.choose_square(board)
   end
 
-  def test_09_computer_will_play_edge_if_forked_on_corner
+  def test_08_computer_will_play_edge_if_forked_on_corner
     board = Board.new
     board = board.board_with_move(1)
     board = board.board_with_move(5)
@@ -83,7 +83,7 @@ class TicTacToeTest < Minitest::Test
   end
 
 
-  def test_10_game_can_be_a_draw
+  def test_09_game_can_be_a_draw
     board = Board.new
     board = board.board_with_move(1)
     board = board.board_with_move(5)
@@ -98,14 +98,14 @@ class TicTacToeTest < Minitest::Test
     assert_equal :draw, board.result
   end
 
-  def test_11_computer_plays_first_open_corner
+  def test_10_computer_plays_first_open_corner
     board = Board.new
     board = board.board_with_move(5)
     board = board.board_with_move(1)
     assert_equal 3, ComputerPlayer.new.choose_square(board)
   end
 
-  def test_12_computer_player_makes_winning_move
+  def test_11_computer_player_makes_winning_move
     board = Board.new
     board = board.board_with_move(5)
     board = board.board_with_move(4)
@@ -114,12 +114,75 @@ class TicTacToeTest < Minitest::Test
     assert_equal 9, ComputerPlayer.new.choose_square(board)
   end
 
-  def test_13_computer_player_blocks_losing_move
+  def test_12_computer_player_blocks_losing_move
     board = Board.new
     board = board.board_with_move(5)
     board = board.board_with_move(1)
     board = board.board_with_move(4)
     assert_equal 6, ComputerPlayer.new.choose_square(board)
   end
+
+  def test_13_square_that_lets_player_win
+    board = Board.new
+    assert_nil ComputerPlayer.new.square_that_lets_player_win(board, :X)
+    assert_nil ComputerPlayer.new.square_that_lets_player_win(board, :O)
+
+    board = board.board_with_move(5)
+    board = board.board_with_move(1)
+    board = board.board_with_move(4)
+    assert_equal 6, ComputerPlayer.new.square_that_lets_player_win(board, :X)
+    assert_nil ComputerPlayer.new.square_that_lets_player_win(board, :O)
+
+    board = board.board_with_move(2)
+    assert_equal 3, ComputerPlayer.new.square_that_lets_player_win(board, :O)
+    assert_equal 6, ComputerPlayer.new.square_that_lets_player_win(board, :X)
+  end
+
+  def test_14_squares_that_let_player_fork
+    board = Board.new
+    assert_equal [], ComputerPlayer.new.squares_that_let_player_fork(board, :X)
+    assert_equal [], ComputerPlayer.new.squares_that_let_player_fork(board, :O)
+
+    board = board.board_with_move(7)
+    board = board.board_with_move(5)
+    board = board.board_with_move(2)
+    assert_equal [1], ComputerPlayer.new.squares_that_let_player_fork(board, :X)
+    assert_equal [], ComputerPlayer.new.squares_that_let_player_fork(board, :O)
+
+    board = Board.new
+    board = board.board_with_move(5)
+    board = board.board_with_move(4)
+    board = board.board_with_move(2)
+    board = board.board_with_move(9)
+    assert_equal [3], ComputerPlayer.new.squares_that_let_player_fork(board, :X)
+    assert_equal [7], ComputerPlayer.new.squares_that_let_player_fork(board, :O)
+
+    board = Board.new
+    board = board.board_with_move(5)
+    board = board.board_with_move(1)
+    board = board.board_with_move(9)
+    assert_equal [3, 6, 7, 8], ComputerPlayer.new.squares_that_let_player_fork(board, :X).sort
+    assert_equal [], ComputerPlayer.new.squares_that_let_player_fork(board, :O)
+
+    board = Board.new
+    board = board.board_with_move(1)
+    board = board.board_with_move(5)
+    board = board.board_with_move(9)
+    assert_equal [3, 7], ComputerPlayer.new.squares_that_let_player_fork(board, :X).sort
+    assert_equal [], ComputerPlayer.new.squares_that_let_player_fork(board, :O)
+  end
+
+  def test_15_open_square_among_squares
+    board = Board.new
+    board = board.board_with_move(1)
+    board = board.board_with_move(4)
+    board = board.board_with_move(2)
+    board = board.board_with_move(9)
+    assert_equal 3, ComputerPlayer.new.open_square_among_squares(board, [1, 3, 6, 7, 9])
+    assert_equal 6, ComputerPlayer.new.open_square_among_squares(board, [2, 4, 6, 8])
+    assert_nil ComputerPlayer.new.open_square_among_squares(board, [1, 4, 2, 9])
+    assert_nil ComputerPlayer.new.open_square_among_squares(board, [])
+  end
+
 
 end
